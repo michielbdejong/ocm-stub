fad3cebccc6fa232ed93fc9cf2ee68b57b8523a3
Parham R <parhamin2010@gmail.com>
2023-04-21 10:47:02 +0000

    add new server to server list token verifier

--- stub.js
+++ stub.js
@@ -274,6 +274,7 @@ const server = https.createServer(HTTPS_OPTIONS, async (req, res) => {
     const queryObject = url.parse(req.url, true).query;
     console.log(queryObject);
         const config = {
+          nc1: "https://nc1.docker/index.php/apps/sciencemesh/accept",
           nc2: "https://nc2.docker/index.php/apps/sciencemesh/accept",
           oc2: "https://oc2.docker/index.php/apps/sciencemesh/accept",
           stub2: "https://stub.docker/ocm/invites/forward",
